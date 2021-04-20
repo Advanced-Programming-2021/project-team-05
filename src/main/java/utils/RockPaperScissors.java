e0b577222c83f9ee82e6840b18f3af6f22fe547f
Mohammad <moh02sadeghi@yahoo.com>
2021-04-20 21:12:31 +0430

    complete Utility class

---
+++ src/main/java/utils/RockPaperScissors.java
@@ -0,0 +1,56 @@
+package utils;
+
+public enum RockPaperScissors {
+    ROCK,
+    PAPER,
+    SCISSORS;
+
+
+    public static void main(String[] args) {
+        System.out.println(RockPaperScissors.valueOf("rock".toUpperCase()));
+
+    }
+
+
+    public static RockPaperScissors getValueOf(String name) {
+        for (RockPaperScissors value : RockPaperScissors.values()) {
+            if (name.toUpperCase().equals(value.name())) {
+                return value;
+            }
+        }
+
+        return null;
+    }
+
+
+    public static int compare(RockPaperScissors firstValue, RockPaperScissors secondValue) {
+        if (firstValue == null || secondValue == null) {
+            return 0;
+        }
+        if (firstValue == ROCK) {
+            if (secondValue == ROCK) {
+                return 0;
+            } else if (secondValue == PAPER) {
+                return 1;
+            } else {
+                return -1;
+            }
+        } else if (firstValue == PAPER) {
+            if (secondValue == ROCK) {
+                return -1;
+            } else if (secondValue == PAPER) {
+                return 0;
+            } else {
+                return 1;
+            }
+        } else {
+            if (secondValue == ROCK) {
+                return 1;
+            } else if (secondValue == PAPER) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+    }
+}
