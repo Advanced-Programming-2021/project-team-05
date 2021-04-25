e3dcc5f474944815c08dd1c3116143547cbc02fb
dshmmd <mohammadzarea.bidaki@gmail.com>
2021-04-25 18:58:26 +0430

    MainMenuController is almost complete.

--- src/main/java/controller/MainMenuController.java
+++ src/main/java/controller/MainMenuController.java
@@ -17,7 +17,35 @@ public class MainMenuController {
     }
 
 
-    public final MainMenuMessage startGame(String opponentUsername, int rounds) {
-        return null;
+    public final MainMenuMessage startGameWithUser(String opponentUsername, int rounds) {
+        // TODO: Complete conditions
+        if (DataManager.getInstance().getUserByUsername(opponentUsername) == null) {
+            return MainMenuMessage.NO_PLAYER_EXISTS;
+        } else if (false) {
+            return MainMenuMessage.NO_ACTIVE_DECK_1;
+        } else if (false) {
+            return MainMenuMessage.NO_ACTIVE_DECK_2;
+        } else if (false) {
+            return MainMenuMessage.INVALID_DECK_1;
+        } else if (false) {
+            return MainMenuMessage.INVALID_DECK_2;
+        } else if (rounds != 3 && rounds != 1) {
+            return MainMenuMessage.INVALID_ROUND;
+        } else {
+            return MainMenuMessage.GAME_STARTED_SUCCESSFULLY;
+        }
+    }
+
+    public final MainMenuMessage startGameWithAi(int rounds) {
+        // TODO: Complete conditions
+        if (false) {
+            return MainMenuMessage.NO_ACTIVE_DECK_1;
+        } else if (false) {
+            return MainMenuMessage.INVALID_DECK_1;
+        } else if (rounds != 3 && rounds != 1) {
+            return MainMenuMessage.INVALID_ROUND;
+        } else {
+            return MainMenuMessage.GAME_STARTED_SUCCESSFULLY;
+        }
     }
 }
