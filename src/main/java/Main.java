bae00dceac7036a6fd6c1f87b9742c27a7ed1e72
dshmmd <mohammadzarea.bidaki@gmail.com>
2021-06-17 21:08:28 +0430

    Bug fixes.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -69,10 +69,10 @@ public class Main {
         manager.addDeck(deckOne);
         manager.addDeck(deckTwo);
 
-        for (int i = 0; i < 59; i++) {
+        for (int i = 0; i < 47; i++) {
             deckOne.addCardToMainDeck(cards.get(i));
         }
-        for (int i = 59; i < 118; i++) {
+        for (int i = 47; i < 94; i++) {
             deckTwo.addCardToMainDeck(cards.get(i));
         }
 
